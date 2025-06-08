e7456f412a18d1f1550d8b0854ef12eb7bb60d08
sandeepr <sandeeprohillainfo@gmail.com>
2025-06-08 15:01:27 +0530

    added best practice code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
@@ -7,4 +8,12 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch(error => {
+    console.log("MONGO db connection failed !!!", err)
+})
