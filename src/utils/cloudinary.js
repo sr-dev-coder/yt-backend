170da3a7d48cee5d9b8cb30bbd9853857924eb64
sandeepr <sandeeprohillainfo@gmail.com>
2025-06-12 21:40:40 +0530

    fix error in cloudinary'

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,7 @@ const uploadOnCloudinary = async (localFilePath) =>{
         if(!localFilePath) return null;
 
         const response = await cloudinary.uploader.upload(localFilePath, { resource_type: "auto" });
-        console.log("file has been upload;", response.url)
+        fs.unlinkSync(localFilePath)
         return response
     }catch(error){
         fs.unlinkSync(localFilePath)
