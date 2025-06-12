e4a28dd8af5afff7214cf32b853f1df0ff5de3d9
sandeepr <sandeeprohillainfo@gmail.com>
2025-06-12 07:11:27 +0530

    fix controller error

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js;"
+import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
@@ -16,7 +16,7 @@ const registerUser = asyncHandler( async (req, res)=>{
     }
     
     //check if user allready exists: username, email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     
@@ -37,7 +37,7 @@ const registerUser = asyncHandler( async (req, res)=>{
     if(!avatar) throw new ApiError(400, "Avatar file is required")
 
     //create user object - create entry in db
-   const user =  User.create({
+   const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
@@ -47,12 +47,16 @@ const registerUser = asyncHandler( async (req, res)=>{
     })
 
     //removed password and refresh token field from response
-    const createdUser = await User.findById(user._id).select("-password -refreshToken")
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
 
     //check for user creation
-    if(!createdUser){
-        throw new ApiError(500, "something went wrong while registering the user")
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
     }
+
+    
 
     //return res
     res.status(201).json(
